6c18b984bbdcb9be078b80255fd17da619cbc553
Calum-W <cal7907@hotmail.co.uk>
2017-11-22 14:45:23 +0000

    ex 11 - Added a dock bike method that bike responds to

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,6 +2,7 @@ require "DockingStation.rb"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+    it { is_expected.to respond_to (:dock).with(1).argument }
 
   it "Creates a new working bike object" do
     bike = subject.release_bike
