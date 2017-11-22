5480fd477f9d8bd73d768df9b0df10afc3296531
Keran <kerbraich@gmail.com>
2017-11-22 15:23:43 +0000

    add attr_accessor to DockingStation class

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,7 +2,16 @@ require "DockingStation.rb"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-    it { is_expected.to respond_to (:dock).with(1).argument }
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
   it "Creates a new working bike object" do
     bike = subject.release_bike
